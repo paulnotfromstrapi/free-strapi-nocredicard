8b5bc82694fb94ab8d06d745ac330c59c72a353d
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2023-09-18 18:05:47 -0500

    initial commit

---
+++ config/env/production/database.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env("DATABASE_URL"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN"),
+        max: env.int("DATABASE_POOL_MAX"),
+      },
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
